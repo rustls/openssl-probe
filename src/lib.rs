d4c29ef999251f666e4387ea941888e18e6f3091
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-11-19 16:52:30 +0100

    Remove trivial wrapper function

--- src/lib.rs
+++ src/lib.rs
@@ -49,26 +49,6 @@ pub fn candidate_cert_dirs() -> impl Iterator<Item = &'static Path> {
 /// Preconfigured values in the environment variables will not be overwritten if the paths they
 /// point to exist and are accessible.
 ///
-/// # Safety
-///
-/// This function is not safe because it mutates the process's environment
-/// variables which is generally not safe. See the [documentation in libstd][doc]
-/// for information about why setting environment variables is not safe.
-///
-/// If possible use the [`probe`] function and directly configure OpenSSL
-/// methods instead of relying on environment variables.
-///
-/// [doc]: https://doc.rust-lang.org/stable/std/env/fn.set_var.html#safety
-pub unsafe fn init_openssl_env_vars() {
-    try_init_openssl_env_vars();
-}
-
-/// Probe for SSL certificates on the system, then configure the SSL certificate `SSL_CERT_FILE`
-/// and `SSL_CERT_DIR` environment variables in this process for OpenSSL to use.
-///
-/// Preconfigured values in the environment variables will not be overwritten if the paths they
-/// point to exist and are accessible.
-///
 /// Returns `true` if any certificate file or directory was found while probing.
 /// Combine this with `has_ssl_cert_env_vars()` to check whether previously configured environment
 /// variables are valid.
